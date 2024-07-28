77b3e093c5a9010bfb848603b26da9372c2075a6
Fer <ferp@protonmail.ch>
2024-07-28 17:20:06 +0200

    optimize via broadphase and sleep

--- src/script.js
+++ src/script.js
@@ -60,6 +60,8 @@ const environmentMapTexture = cubeTextureLoader.load([
  */
 // World
 const world = new CANNON.World();
+world.broadphase = new CANNON.SAPBroadphase(world);
+world.allowSleep = true;
 world.gravity.set(0, -9.82, 0); // is is a Vec3 not a Vector3 (Cannon.js)
 
 // Materials  - physics
